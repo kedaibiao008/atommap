b4db06ee60575221e30e5d92d67aca5fcce05313
kedaibiao008 <118370965+kedaibiao008@users.noreply.github.com>
2023-10-28 22:12:32 +0800

    Update 99.py

--- 99.py
+++ 99.py
@@ -1,11 +1,11 @@
 import os
 
 # 指定文件名和文本内容
-filename = os.path.expanduser("~/atomicals/atomicals-js/6667.atommap.svg")  # 包含完整路径的文件名
+filename = os.path.expanduser("~/atomicals/atomicals-js/6664.atommap.svg")  # 包含完整路径的文件名
 svg_content = """
 <svg width="200" height="50" xmlns="http://www.w3.org/2000/svg">
     <rect width="100%" height="100%" fill="#ffffff"/>
-    <text x="50%" y="50%" font-family="Arial" font-size="20" fill="black" dominant-baseline="middle" text-anchor="middle">6667.atommap</text>
+    <text x="50%" y="50%" font-family="Arial" font-size="20" fill="black" dominant-baseline="middle" text-anchor="middle">6664.atommap</text>
 </svg>
 """
 
