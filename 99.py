9defb3bcd348e4cdd7da92c9b09d292dfd3e763b
kedaibiao008 <118370965+kedaibiao008@users.noreply.github.com>
2023-10-28 21:35:41 +0800

    Create 99.py

---
+++ 99.py
@@ -0,0 +1,14 @@
+# 指定文件名和文本内容
+filename = "9993.atommap.svg"
+svg_content = """
+<svg width="200" height="50" xmlns="http://www.w3.org/2000/svg">
+    <rect width="100%" height="100%" fill="#ffffff"/>
+    <text x="50%" y="50%" font-family="Arial" font-size="20" fill="black" dominant-baseline="middle" text-anchor="middle">9993.atommap</text>
+</svg>
+"""
+
+# 将文本内容写入文件
+with open(filename, "w") as file:
+    file.write(svg_content)
+
+print(f"文件 '{filename}' 已成功创建！")
